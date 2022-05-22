a0d967c532e8322f91adba01bb2c6d66fceecb9b
ouakassi <lmoutchoo@gmail.com>
2022-05-22 01:16:00 +0100

    adding FAQ section and its functionality usin javascript

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,24 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".arrow-left",
   },
 });
+
+const box = document.querySelectorAll(".box");
+const boxContent = document.querySelectorAll(".box-content");
+const icon = document.querySelectorAll(".box-head i");
+const firstBoxContent = document.querySelectorAll(".box-content")[0];
+const showBoxContent = "active";
+
+box.forEach((e, i) => {
+  e.addEventListener("click", () => {
+    if (!boxContent[i].classList.contains(showBoxContent)) {
+      boxContent[i].classList.add(showBoxContent);
+      boxContent[i].style.height = boxContent[i].scrollHeight + "px";
+      icon[i].classList.replace("uil-angle-down", "uil-angle-up");
+    } else {
+      boxContent[i].classList.remove(showBoxContent);
+      boxContent[i].style.height = 0;
+      icon[i].classList.replace("uil-angle-up", "uil-angle-down");
+    }
+    console.log(e);
+  });
+});
